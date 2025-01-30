4bff2364c852db58f7e37f5bbd86631e7764a7d0
ashlyochwada <ashly@carble.co>
2025-01-30 17:11:37 +0300

    removed the pulled conversion code

--- src/components/nav-pages/dashboard/Dashboard.tsx
+++ src/components/nav-pages/dashboard/Dashboard.tsx
@@ -12,6 +12,7 @@ import { GeoJsonObject } from 'geojson';
 import { mapLayers } from "@/components/constants";
 import SidebarItems from "@/components/nav-pages/dashboard/SidebarItems";
 import SearchControl from "@/components/nav-pages/dashboard/db_functions";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@/components/ui/dropdown-menu";
 
 interface MapProps {
     geoJSONDataList?: GeoJsonObject[];
@@ -62,15 +63,10 @@ const DashboardMap: React.FC<MapProps> = () => {
         });
     }, [geoJSONDataList]);
 
-    // Convert GeoJSON to EPSG:3857 (Web Mercator) for Leaflet
-    const convertToEPSG3857 = (geoJSON: GeoJsonObject) => {
-        if (!("features" in geoJSON)) return geoJSON;
-        return toMercator(featureCollection(geoJSON.features as Feature<Geometry, GeoJsonProperties>[]));
-    };
+  
 
     return (
         <div className="flex h-[calc(100vh-5rem)] overflow-hidden w-full">
-            {/* Sidebar - Ensure it's always visible */}
             <div
                 className={`transition-all duration-300 flex flex-col border-r border-gray-200/[0.25] bg-white z-50 ${sidebarOpen ? "w-64" : "min-w-12"}`}
             >
